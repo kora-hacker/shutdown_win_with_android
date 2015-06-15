493d7d7a9ce128cff5e68cf733a879360a960f28
kora-hacker <kora.hacker@qq.com>
2015-06-15 20:32:34 +0800

    add project "Shutdown.ServiceSelf"
    add project "Shutdown.ServiceSelf"

---
+++ Shutdown.ServiceSelf/Shutdown.ServiceSelf/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shutdown.ServiceSelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0 && args[0] == "s")
+            {
+                ServiceBase[] service_to_run;
+                service_to_run = new ServiceBase[]
+                {
+                    new ServiceHost(),
+                };
+                ServiceBase.Run(service_to_run);
+            }
+            else
+            {
+                Console.WriteLine("这是Windows应用程序");
+                Console.WriteLine("请选择，[1]安装服务 [2]卸载服务 [3]退出");
+                var rs = int.Parse(Console.ReadLine());
+                switch (rs)
+                {
+                    case 1:
+                        //取当前可执行文件路径，加上"s"参数，证明是从windows服务启动该程序
+                        var path = Process.GetCurrentProcess().MainModule.FileName + " s";
+                        Process.Start("sc", "create myserver binpath= \"" + path + "\" displayName= ShutdownServiceSelfServer start= auto");
+                        Console.WriteLine("安装成功");
+                        Console.Read();
+                        break;
+                    case 2:
+                        Process.Start("sc", "delete myserver");
+                        Console.WriteLine("卸载成功");
+                        Console.Read();
+                        break;
+                    case 3: break;
+                }
+            }
+        }
+    }
+}
---
+++ Shutdown.ServiceSelf/Shutdown.ServiceSelf/ServiceHost.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace Shutdown.ServiceSelf
+{
+    partial class ServiceHost : ServiceBase
+    {
+        public string log_file_path = "C:\\Users\\ismdeep\\shutdown_log.txt";
+        public string config_file_path = "C:\\Users\\ismdeep\\shutdown_service_self\\config\\";
+        public ServiceHost()
+        {
+            InitializeComponent();
+        }
+
+        public string shift_string (string str)
+        {
+            string ans = "";
+            bool flag = true;
+            for (int i = 0; flag && i < str.Length; i++)
+            {
+                if (str[i] == 0)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    ans += str[i];
+                }
+            }
+            return ans;
+        }
+
+        public string[] read_command_from_config_file (string config_id)
+        {
+            // File command_file = new File(config_file_path + config_id + ".txt");
+            string[] command_line = File.ReadAllLines(config_file_path + config_id + ".txt");
+            return command_line;
+        }
+
+        public void NoneThread ()
+        {
+
+            TcpListener listener = new TcpListener(IPAddress.Any, 12354);
+            listener.Start();
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                NetworkStream client_stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                int read_cnt = 0;
+                read_cnt = client_stream.Read(buffer, 0, 1024);
+                string str = System.Text.Encoding.ASCII.GetString(buffer);
+                str = shift_string(str);
+                // System.Console.WriteLine(str);
+                System.IO.File.AppendAllText(log_file_path, str + "\r\n");
+
+
+                string[] _command_ = read_command_from_config_file(str);
+
+                
+                    // ---- Process.Start("cmd.exe", _command_[i]);
+                Process p = new Process();
+                p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.CreateNoWindow = true;
+                p.Start();
+
+                for (int i = 0; i < _command_.Length; i++)
+                {
+                    System.IO.File.AppendAllText(log_file_path, ">> " + _command_[i] + "\r\n");
+                    p.StandardInput.WriteLine(_command_[i]);
+                }
+
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // TODO: Add code here to start your service.
+            System.IO.File.AppendAllText(log_file_path, "Service Is Started……" + DateTime.Now.ToString() + "\r\n");
+            ServiceHost p = new ServiceHost();
+            Thread t = new Thread(new ThreadStart(p.NoneThread));
+            t.Start();
+            // ---- System.IO.File.AppendAllText(log_file_path, Convert.ToString(  read_command_from_config_file("4").Length  ));
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: Add code here to perform any tear-down necessary to stop your service.
+            System.IO.File.AppendAllText(log_file_path, "Service Is Stopped……" + DateTime.Now.ToString() + "\r\n");
+        }
+    }
+}
